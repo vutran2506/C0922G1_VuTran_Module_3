4133f35ecb24cb45ea8e38720432465f44383949
vutran2506 <tranngocvu1832@gmail.com>
2022-12-14 15:03:42 +0700

    nop bai tap ss1

---
+++ mysql/ss1_relational_database/exercise/student_management.sql
@@ -0,0 +1,12 @@
+create database student_management;
+use student_management;classteacher
+create table class(
+id int,
+name varchar(50)
+);
+create table teacher(
+id int,
+name varchar(150),
+age int,
+country varchar(150)
+);
