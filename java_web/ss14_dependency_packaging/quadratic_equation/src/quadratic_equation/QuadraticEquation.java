11ee02cebcbe825decdcf553db2117635d704c5f
vutran2506 <tranngocvu1832@gmail.com>
2023-01-06 11:40:17 +0700

    nop bai tap ss14

---
+++ java_web/ss14_dependency_packaging/quadratic_equation/src/quadratic_equation/QuadraticEquation.java
@@ -0,0 +1,22 @@
+package quadratic_equation;
+
+public class QuadraticEquation {
+
+
+
+    public static void quadratic(int a, int b, int c) {
+        double x1, x2;
+        double delta = (b * b) - (4 * a * c);
+        if (delta < 0) {
+            System.out.println("The equation has no solution");
+        } else if (delta == 0) {
+            x1 = x2 = -b / (2 * a);
+            System.out.println("x1 và x2 =" + x1);
+        } else {
+            x1 = (-b - Math.sqrt(delta)) / 2 * a;
+            x2 = (-b + Math.sqrt(delta)) / 2 * a;
+            System.out.println("x1 " + x1 + " and " + " x2 " + x2);
+        }
+
+    }
+}
